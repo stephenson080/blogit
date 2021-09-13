65de24d3b21afd1b6c405b64ec9c45464e45de07
stevepathagoras <stevepathagoras08@gmail.com>
2021-09-14 00:07:34 +0100

    Update db

--- database/dbConnection.js
+++ database/dbConnection.js
@@ -3,7 +3,10 @@ const {Sequelize} = require("sequelize")
 const sequelize = new Sequelize("dc2lsfh782hm3f", "ucomddhlcdjqxh", "9496a84d6cabc2b25dd755ddac0eae4135cb23a46579269901d9cffc644fc041", {
     dialect: "postgres",
     host: "ec2-52-45-238-24.compute-1.amazonaws.com",
-    port: "5432"
+    port: "5432",
+    ssl: {
+        rejectUnauthorized: false
+    }
 })
 // const {Sequelize} = require("sequelize")
 
