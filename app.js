ae240753ad2f8d1935f947458512a9a9d369559c
stevepathagoras <stevepathagoras08@gmail.com>
2021-09-15 14:55:54 +0100

    'Okay'

--- app.js
+++ app.js
@@ -159,8 +159,8 @@ app.use(get404Page)
 
 // apps entry point
 dbConnection
-    .sync({force: true})
-    // .sync()
+    // .sync({force: true})
+    .sync()
     .then(async res => {
         app.listen(PORT || 3000, function () {
             console.log(`server running on ${PORT}`)
